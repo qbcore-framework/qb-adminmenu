6fc9709fd87c37d0a0876a1bce76b4585b0f7a5d
ChatDisabled <44729807+ChatDisabled@users.noreply.github.com>
2021-08-06 17:03:58 +0200

    Bear being lazy

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -4,6 +4,8 @@ game 'gta5'
 description 'QB-AdminMenu'
 version '1.0.0'
 
+ui_page 'html/index.html'
+
 client_scripts {
     '@menuv/menuv.lua',
     'client/noclip.lua',
@@ -12,9 +14,14 @@ client_scripts {
     'client/events.lua'
 }
 
-shared_script '@qb-core/import.lua'
+shared_script '@ds-core/import.lua'
 
 server_script 'server/server.lua'
+
+files { -- Credits to https://github.com/LVRP-BEN/bl_coords for clipboard copy method
+    'html/index.html',
+    'html/index.js'
+}
 
 dependencies {
     'menuv'
