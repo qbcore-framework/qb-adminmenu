f403ffb0d08228025293eee01c45b09eb6d455f5
RSG -Realstonergamer <xanten11@gmail.com>
2022-08-03 16:18:49 -0400

    fixed wrong patch
    this is the right patch sorry

--- client/events.lua
+++ client/events.lua
@@ -156,12 +156,12 @@ function PerformanceUpgradeVehicle(vehicle, customWheels)
     customWheels = customWheels or false
     local max
     if DoesEntityExist(vehicle) and IsEntityAVehicle(vehicle) then
+        SetVehicleModKit(vehicle, 0)
         for _, modType in ipairs(performanceModIndices) do
-            max = GetNumVehicleMods(vehicle, modType) - 1
+            max = GetNumVehicleMods(vehicle, tonumber(modType)) - 1
             SetVehicleMod(vehicle, modType, max, customWheels)
         end
         ToggleVehicleMod(vehicle, 18, true) -- Turbo
-        SetVehicleFixed(vehicle)
     end
 end
 
@@ -169,3 +169,4 @@ RegisterNetEvent('qb-admin:client:maxmodVehicle', function()
     local vehicle = GetVehiclePedIsIn(PlayerPedId())
     PerformanceUpgradeVehicle(vehicle)
 end)
+
