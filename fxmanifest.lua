ac16773efe77da592d2bb6b63ab655cab6d07e7a
Kakarot <57848836+GhzGarage@users.noreply.github.com>
2024-02-11 05:54:29 -0600

    Description for txAdmin

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,7 +1,8 @@
 fx_version 'cerulean'
 game 'gta5'
 lua54 'yes'
-description 'QB-AdminMenu'
+author 'Kakarot'
+description 'Provides a menu for server and player management'
 version '1.2.0'
 
 ui_page 'html/index.html'
