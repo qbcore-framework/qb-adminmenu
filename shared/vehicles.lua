f745ec35a60a6715c49a44e56fa9f603db319121
Adharsh M <adhershmnair@gmail.com>
2024-11-17 15:39:15 +0530

    Added Vechicle modification configurations.

---
+++ shared/vehicles.lua
@@ -0,0 +1,1055 @@
+VehicleMods = {};
+VehicleMods.enabled = true;
+VehicleMods.FuelResource = "LegacyFuel";
+VehicleMods.colors = {
+	{
+		name = "Black",
+		colorindex = 0
+	},
+	{
+		name = "Carbon Black",
+		colorindex = 147
+	},
+	{
+		name = "Graphite",
+		colorindex = 1
+	},
+	{
+		name = "Anthracite Black",
+		colorindex = 11
+	},
+	{
+		name = "Black Steel",
+		colorindex = 2
+	},
+	{
+		name = "Dark Steel",
+		colorindex = 3
+	},
+	{
+		name = "Silver",
+		colorindex = 4
+	},
+	{
+		name = "Bluish Silver",
+		colorindex = 5
+	},
+	{
+		name = "Rolled Steel",
+		colorindex = 6
+	},
+	{
+		name = "Shadow Silver",
+		colorindex = 7
+	},
+	{
+		name = "Stone Silver",
+		colorindex = 8
+	},
+	{
+		name = "Midnight Silver",
+		colorindex = 9
+	},
+	{
+		name = "Cast Iron Silver",
+		colorindex = 10
+	},
+	{
+		name = "Red",
+		colorindex = 27
+	},
+	{
+		name = "Torino Red",
+		colorindex = 28
+	},
+	{
+		name = "Formula Red",
+		colorindex = 29
+	},
+	{
+		name = "Lava Red",
+		colorindex = 150
+	},
+	{
+		name = "Blaze Red",
+		colorindex = 30
+	},
+	{
+		name = "Grace Red",
+		colorindex = 31
+	},
+	{
+		name = "Garnet Red",
+		colorindex = 32
+	},
+	{
+		name = "Sunset Red",
+		colorindex = 33
+	},
+	{
+		name = "Cabernet Red",
+		colorindex = 34
+	},
+	{
+		name = "Wine Red",
+		colorindex = 143
+	},
+	{
+		name = "Candy Red",
+		colorindex = 35
+	},
+	{
+		name = "Hot Pink",
+		colorindex = 135
+	},
+	{
+		name = "Pfsiter Pink",
+		colorindex = 137
+	},
+	{
+		name = "Salmon Pink",
+		colorindex = 136
+	},
+	{
+		name = "Sunrise Orange",
+		colorindex = 36
+	},
+	{
+		name = "Orange",
+		colorindex = 38
+	},
+	{
+		name = "Bright Orange",
+		colorindex = 138
+	},
+	{
+		name = "Gold",
+		colorindex = 99
+	},
+	{
+		name = "Bronze",
+		colorindex = 90
+	},
+	{
+		name = "Yellow",
+		colorindex = 88
+	},
+	{
+		name = "Race Yellow",
+		colorindex = 89
+	},
+	{
+		name = "Dew Yellow",
+		colorindex = 91
+	},
+	{
+		name = "Dark Green",
+		colorindex = 49
+	},
+	{
+		name = "Racing Green",
+		colorindex = 50
+	},
+	{
+		name = "Sea Green",
+		colorindex = 51
+	},
+	{
+		name = "Olive Green",
+		colorindex = 52
+	},
+	{
+		name = "Bright Green",
+		colorindex = 53
+	},
+	{
+		name = "Gasoline Green",
+		colorindex = 54
+	},
+	{
+		name = "Lime Green",
+		colorindex = 92
+	},
+	{
+		name = "Midnight Blue",
+		colorindex = 141
+	},
+	{
+		name = "Galaxy Blue",
+		colorindex = 61
+	},
+	{
+		name = "Dark Blue",
+		colorindex = 62
+	},
+	{
+		name = "Saxon Blue",
+		colorindex = 63
+	},
+	{
+		name = "Blue",
+		colorindex = 64
+	},
+	{
+		name = "Mariner Blue",
+		colorindex = 65
+	},
+	{
+		name = "Harbor Blue",
+		colorindex = 66
+	},
+	{
+		name = "Diamond Blue",
+		colorindex = 67
+	},
+	{
+		name = "Surf Blue",
+		colorindex = 68
+	},
+	{
+		name = "Nautical Blue",
+		colorindex = 69
+	},
+	{
+		name = "Racing Blue",
+		colorindex = 73
+	},
+	{
+		name = "Ultra Blue",
+		colorindex = 70
+	},
+	{
+		name = "Light Blue",
+		colorindex = 74
+	},
+	{
+		name = "Chocolate Brown",
+		colorindex = 96
+	},
+	{
+		name = "Bison Brown",
+		colorindex = 101
+	},
+	{
+		name = "Creeen Brown",
+		colorindex = 95
+	},
+	{
+		name = "Feltzer Brown",
+		colorindex = 94
+	},
+	{
+		name = "Maple Brown",
+		colorindex = 97
+	},
+	{
+		name = "Beechwood Brown",
+		colorindex = 103
+	},
+	{
+		name = "Sienna Brown",
+		colorindex = 104
+	},
+	{
+		name = "Saddle Brown",
+		colorindex = 98
+	},
+	{
+		name = "Moss Brown",
+		colorindex = 100
+	},
+	{
+		name = "Woodbeech Brown",
+		colorindex = 102
+	},
+	{
+		name = "Straw Brown",
+		colorindex = 99
+	},
+	{
+		name = "Sandy Brown",
+		colorindex = 105
+	},
+	{
+		name = "Bleached Brown",
+		colorindex = 106
+	},
+	{
+		name = "Schafter Purple",
+		colorindex = 71
+	},
+	{
+		name = "Spinnaker Purple",
+		colorindex = 72
+	},
+	{
+		name = "Midnight Purple",
+		colorindex = 142
+	},
+	{
+		name = "Bright Purple",
+		colorindex = 145
+	},
+	{
+		name = "Cream",
+		colorindex = 107
+	},
+	{
+		name = "Ice White",
+		colorindex = 111
+	},
+	{
+		name = "Frost White",
+		colorindex = 112
+	}
+};
+VehicleMods.metalcolors = {
+	{
+		name = "Brushed Steel",
+		colorindex = 117
+	},
+	{
+		name = "Brushed Black Steel",
+		colorindex = 118
+	},
+	{
+		name = "Brushed Aluminum",
+		colorindex = 119
+	},
+	{
+		name = "Pure Gold",
+		colorindex = 158
+	},
+	{
+		name = "Brushed Gold",
+		colorindex = 159
+	},
+	{
+		name = "Chrome",
+		colorindex = 120
+	}
+};
+VehicleMods.mattecolors = {
+	{
+		name = "Black",
+		colorindex = 12
+	},
+	{
+		name = "Gray",
+		colorindex = 13
+	},
+	{
+		name = "Light Gray",
+		colorindex = 14
+	},
+	{
+		name = "Ice White",
+		colorindex = 131
+	},
+	{
+		name = "Blue",
+		colorindex = 83
+	},
+	{
+		name = "Dark Blue",
+		colorindex = 82
+	},
+	{
+		name = "Midnight Blue",
+		colorindex = 84
+	},
+	{
+		name = "Midnight Purple",
+		colorindex = 149
+	},
+	{
+		name = "Schafter Purple",
+		colorindex = 148
+	},
+	{
+		name = "Red",
+		colorindex = 39
+	},
+	{
+		name = "Dark Red",
+		colorindex = 40
+	},
+	{
+		name = "Orange",
+		colorindex = 41
+	},
+	{
+		name = "Yellow",
+		colorindex = 42
+	},
+	{
+		name = "Lime Green",
+		colorindex = 55
+	},
+	{
+		name = "Green",
+		colorindex = 128
+	},
+	{
+		name = "Frost Green",
+		colorindex = 151
+	},
+	{
+		name = "Foliage Green",
+		colorindex = 155
+	},
+	{
+		name = "Olive Darb",
+		colorindex = 152
+	},
+	{
+		name = "Dark Earth",
+		colorindex = 153
+	},
+	{
+		name = "Desert Tan",
+		colorindex = 154
+	}
+};
+VehicleMods.tyrecolors = {
+	{
+		name = "Black",
+		r = 0,
+		g = 0,
+		b = 0
+	},
+	{
+		name = "White\t",
+		r = 255,
+		g = 255,
+		b = 255
+	},
+	{
+		name = "Red",
+		r = 255,
+		g = 0,
+		b = 0
+	},
+	{
+		name = "Lime",
+		r = 0,
+		g = 255,
+		b = 0
+	},
+	{
+		name = "Blue",
+		r = 0,
+		g = 0,
+		b = 255
+	},
+	{
+		name = "Yellow",
+		r = 255,
+		g = 255,
+		b = 0
+	},
+	{
+		name = "Cyan",
+		r = 0,
+		g = 255,
+		b = 255
+	},
+	{
+		name = "Magenta",
+		r = 255,
+		g = 0,
+		b = 255
+	},
+	{
+		name = "Silver",
+		r = 192,
+		g = 192,
+		b = 192
+	},
+	{
+		name = "Gray",
+		r = 128,
+		g = 128,
+		b = 128
+	},
+	{
+		name = "Maroon",
+		r = 128,
+		g = 0,
+		b = 0
+	},
+	{
+		name = "Olive",
+		r = 128,
+		g = 128,
+		b = 0
+	},
+	{
+		name = "Green",
+		r = 0,
+		g = 128,
+		b = 0
+	},
+	{
+		name = "Purple",
+		r = 128,
+		g = 0,
+		b = 128
+	},
+	{
+		name = "Green",
+		r = 0,
+		g = 128,
+		b = 0
+	},
+	{
+		name = "Teal",
+		r = 0,
+		g = 128,
+		b = 128
+	},
+	{
+		name = "Navy Blue",
+		r = 0,
+		g = 0,
+		b = 128
+	}
+};
+VehicleMods.neoncolors = {
+	{
+		label = "White",
+		value = {
+			222,
+			222,
+			255
+		},
+		description = "Demo Item 1"
+	},
+	{
+		label = "Blue",
+		value = {
+			2,
+			21,
+			255
+		},
+		description = "Demo Item 2"
+	},
+	{
+		label = "Electric Blue",
+		value = {
+			3,
+			83,
+			255
+		},
+		description = "Demo Item 3"
+	},
+	{
+		label = "Mint Green",
+		value = {
+			0,
+			222,
+			140
+		},
+		description = "Demo Item 4"
+	},
+	{
+		label = "Lime Green",
+		value = {
+			94,
+			222,
+			1
+		},
+		description = "Demo Item 4"
+	},
+	{
+		label = "Yellow",
+		value = {
+			255,
+			255,
+			0
+		},
+		description = "Demo Item 4"
+	},
+	{
+		label = "Golden Shower",
+		value = {
+			255,
+			150,
+			0
+		},
+		description = "Demo Item 4"
+	},
+	{
+		label = "Orange",
+		value = {
+			255,
+			62,
+			0
+		},
+		description = "Demo Item 4"
+	},
+	{
+		label = "Red",
+		value = {
+			255,
+			1,
+			1
+		},
+		description = "Demo Item 4"
+	},
+	{
+		label = "Pony Pink",
+		value = {
+			255,
+			50,
+			100
+		},
+		description = "Demo Item 4"
+	},
+	{
+		label = "Hot Pink",
+		value = {
+			255,
+			5,
+			190
+		},
+		description = "Demo Item 4"
+	},
+	{
+		label = "Purple",
+		value = {
+			35,
+			1,
+			255
+		},
+		description = "Demo Item 4"
+	},
+	{
+		label = "Blacklight",
+		value = {
+			15,
+			3,
+			255
+		},
+		description = "Demo Item 4"
+	}
+};
+VehicleMods.vehmods = {
+	modTurbo = {
+		label = "Turbo",
+		parent = "upgrades",
+		modType = 17,
+		price = {
+			55.81
+		}
+	},
+	modEngine = {
+		label = "Engine",
+		parent = "upgrades",
+		modType = 11,
+		price = {
+			13.95,
+			32.56,
+			65.12,
+			139.53
+		}
+	},
+	modBrakes = {
+		label = "Brakes",
+		parent = "upgrades",
+		modType = 12,
+		price = {
+			4.65,
+			9.3,
+			18.6,
+			13.95
+		}
+	},
+	modTransmission = {
+		label = "Transmission",
+		parent = "upgrades",
+		modType = 13,
+		price = {
+			13.95,
+			20.93,
+			46.51
+		}
+	},
+	modSuspension = {
+		label = "Suspension",
+		parent = "upgrades",
+		modType = 15,
+		price = {
+			3.72,
+			7.44,
+			14.88,
+			29.77,
+			40.2
+		}
+	},
+	modArmor = {
+		label = "Armor",
+		parent = "upgrades",
+		modType = 16,
+		price = {
+			69.77,
+			116.28,
+			130,
+			150,
+			180,
+			190
+		}
+	},
+	modPlateHolder = {
+		label = "Plate - Back",
+		parent = "cosmetics",
+		modType = 25,
+		price = 3.49
+	},
+	modVanityPlate = {
+		label = "Plate - Front",
+		parent = "cosmetics",
+		modType = 26,
+		price = 1.1
+	},
+	modTrimA = {
+		label = "Interior",
+		parent = "cosmetics",
+		modType = 27,
+		price = 6.98
+	},
+	modOrnaments = {
+		label = "Trim",
+		parent = "cosmetics",
+		modType = 28,
+		price = 0.9
+	},
+	modDashboard = {
+		label = "Dashboard",
+		parent = "cosmetics",
+		modType = 29,
+		price = 4.65
+	},
+	modDial = {
+		label = "Speedometer",
+		parent = "cosmetics",
+		modType = 30,
+		price = 4.19
+	},
+	modDoorSpeaker = {
+		label = "Door Speakers",
+		parent = "cosmetics",
+		modType = 31,
+		price = 5.58
+	},
+	modSeats = {
+		label = "Seats",
+		parent = "cosmetics",
+		modType = 32,
+		price = 4.65
+	},
+	modSteeringWheel = {
+		label = "Steering Wheel",
+		parent = "cosmetics",
+		modType = 33,
+		price = 4.19
+	},
+	modShifterLeavers = {
+		label = "Gear Lever",
+		parent = "cosmetics",
+		modType = 34,
+		price = 3.26
+	},
+	modAPlate = {
+		label = "Quarter-Deck",
+		parent = "cosmetics",
+		modType = 35,
+		price = 4.19
+	},
+	modSpeakers = {
+		label = "Speakers",
+		parent = "cosmetics",
+		modType = 36,
+		price = 6.98
+	},
+	modTrunk = {
+		label = "Trunk",
+		parent = "cosmetics",
+		modType = 37,
+		price = 5.58
+	},
+	modHydrolic = {
+		label = "Hydraulic",
+		parent = "cosmetics",
+		modType = 38,
+		price = 5.12
+	},
+	modEngineBlock = {
+		label = "Engine Block",
+		parent = "cosmetics",
+		modType = 39,
+		price = 5.12
+	},
+	modAirFilter = {
+		label = "Air Filter",
+		parent = "cosmetics",
+		modType = 40,
+		price = 3.72
+	},
+	modStruts = {
+		label = "Struts",
+		parent = "cosmetics",
+		modType = 41,
+		price = 6.51
+	},
+	modArchCover = {
+		label = "Arch Cover",
+		parent = "cosmetics",
+		modType = 42,
+		price = 4.19
+	},
+	modAerials = {
+		label = "Aerials",
+		parent = "cosmetics",
+		modType = 43,
+		price = 1.12
+	},
+	modTrimB = {
+		label = "Wings",
+		parent = "cosmetics",
+		modType = 44,
+		price = 6.05
+	},
+	modTank = {
+		label = "Fuel Tank",
+		parent = "cosmetics",
+		modType = 45,
+		price = 4.19
+	},
+	modWindows = {
+		label = "Windows",
+		parent = "cosmetics",
+		modType = 46,
+		price = 4.19
+	},
+	modLivery = {
+		label = "Stickers",
+		parent = "cosmetics",
+		modType = 48,
+		price = 9.3
+	},
+	modFrontWheelsType0 = {
+		label = "Sport",
+		parent = "modFrontWheelsTypes",
+		modType = 23,
+		wheelType = 0,
+		price = 4.65
+	},
+	modFrontWheelsType1 = {
+		label = "Muscle",
+		parent = "modFrontWheelsTypes",
+		modType = 23,
+		wheelType = 1,
+		price = 4.19
+	},
+	modFrontWheelsType2 = {
+		label = "Low Rider",
+		parent = "modFrontWheelsTypes",
+		modType = 23,
+		wheelType = 2,
+		price = 4.65
+	},
+	modFrontWheelsType3 = {
+		label = "SUV",
+		parent = "modFrontWheelsTypes",
+		modType = 23,
+		wheelType = 3,
+		price = 4.19
+	},
+	modFrontWheelsType4 = {
+		label = "All Terrain",
+		parent = "modFrontWheelsTypes",
+		modType = 23,
+		wheelType = 4,
+		price = 4.19
+	},
+	modFrontWheelsType5 = {
+		label = "Tuning",
+		parent = "modFrontWheelsTypes",
+		modType = 23,
+		wheelType = 5,
+		price = 5.12
+	},
+	modFrontWheelsType6 = {
+		label = "Motorcycle",
+		parent = "modFrontWheelsTypes",
+		modType = 23,
+		wheelType = 6,
+		price = 3.26
+	},
+	modFrontWheelsType7 = {
+		label = "Highend",
+		parent = "modFrontWheelsTypes",
+		modType = 23,
+		wheelType = 7,
+		price = 5.12
+	},
+	wheelColor = {
+		label = "Wheel Color",
+		parent = "modFrontWheelsColor",
+		modType = "wheelColor",
+		price = 0.66
+	},
+	plateIndex = {
+		label = "License Plates",
+		parent = "cosmetics",
+		modType = "plateIndex",
+		price = 1.1
+	},
+	color1 = {
+		label = "Primary Color",
+		parent = "primaryRespray",
+		modType = "color1",
+		price = 1.12
+	},
+	color2 = {
+		label = "Secondary Color",
+		parent = "secondaryRespray",
+		modType = "color2",
+		price = 0.66
+	},
+	pearlescentColor = {
+		label = "Pearlescent Color",
+		parent = "pearlescentRespray",
+		modType = "pearlescentColor",
+		price = 0.88
+	},
+	modXenon = {
+		label = "Headlight",
+		parent = "cosmetics",
+		modType = 22,
+		price = 3.72
+	},
+	modSpoilers = {
+		label = "Spoilers",
+		parent = "bodyparts",
+		modType = 0,
+		price = 4.65
+	},
+	modFrontBumper = {
+		label = "Front Bumper",
+		parent = "bodyparts",
+		modType = 1,
+		price = 5.12
+	},
+	modRearBumper = {
+		label = "Rear Bumper",
+		parent = "bodyparts",
+		modType = 2,
+		price = 5.12
+	},
+	modSideSkirt = {
+		label = "Sideskirt",
+		parent = "bodyparts",
+		modType = 3,
+		price = 4.65
+	},
+	modExhaust = {
+		label = "Exhaust",
+		parent = "bodyparts",
+		modType = 4,
+		price = 5.12
+	},
+	modFrame = {
+		label = "Cage",
+		parent = "bodyparts",
+		modType = 5,
+		price = 5.12
+	},
+	modGrille = {
+		label = "Grille",
+		parent = "bodyparts",
+		modType = 6,
+		price = 3.72
+	},
+	modHood = {
+		label = "Hood",
+		parent = "bodyparts",
+		modType = 7,
+		price = 4.88
+	},
+	modFender = {
+		label = "Left Fender",
+		parent = "bodyparts",
+		modType = 8,
+		price = 5.12
+	},
+	modRightFender = {
+		label = "Right Fender",
+		parent = "bodyparts",
+		modType = 9,
+		price = 5.12
+	},
+	modRoof = {
+		label = "Roof",
+		parent = "bodyparts",
+		modType = 10,
+		price = 5.58
+	},
+	modHorns = {
+		label = "Horns",
+		parent = "cosmetics",
+		modType = 14,
+		price = 1.12
+	},
+	neonColor = {
+		label = "Neon",
+		parent = "cosmetics",
+		modType = "neonColor",
+		price = 1.12
+	},
+	tyreSmokeColor = {
+		label = "Tiresmoke",
+		parent = "wheels",
+		modType = "tyreSmokeColor",
+		price = 1.12
+	},
+	modSticker = {
+		label = "Livery 2",
+		parent = "cosmetics",
+		modType = "livery2",
+		price = 9.3
+	}
+};
+VehicleMods.vehicledoors = {
+	{
+		label = "LFront Door",
+		value = 0,
+		description = "Left Front Door"
+	},
+	{
+		label = "RFront Door",
+		value = 1,
+		description = "Right Front Door"
+	},
+	{
+		label = "LRear Door",
+		value = 2,
+		description = "Left Rear Door"
+	},
+	{
+		label = "RRear Door",
+		value = 3,
+		description = "Right Rear Door"
+	},
+	{
+		label = "Hood",
+		value = 4,
+		description = "Hood"
+	},
+	{
+		label = "Trunk",
+		value = 5,
+		description = "Trunk"
+	}
+};
